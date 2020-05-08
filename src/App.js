b0aa2908bf2ca5c3d61aab942c878d78ffd7b3f6
kenta <3ks4giving@gmail.com>
2020-05-08 21:44:45 +0900

    Hello Worldを追加

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
+          <br></br>
+          Hello World!
         </p>
         <a
           className="App-link"
